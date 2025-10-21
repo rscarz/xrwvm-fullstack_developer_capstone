f836a36e5e82d6d3dd602d22228cf75b3cd8f5a2
rscarz <rosa.scarz@gmail.com>
2025-10-21 10:59:59 -0400

    hasta get dealets

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -8,10 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="https://rosabenitez-3030.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+    'backend_url', default="https://rosabenitez-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="https://sentianalyzer.21jg2pw9sy6n.us-south.codeengine.appdomain.cloud")
+    default="https://sentianalyzer.21rznzm3rz0v.us-south.codeengine.appdomain.cloud")
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,13 +1,17 @@
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<RegisterPanel />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<RegisterPanel />} />
+            <Route path="/dealers" element={<Dealers />} />
+            <Route path="/dealer/:id" element={<Dealer/>} />
+        </Routes>
+    );
 }
 export default App;
